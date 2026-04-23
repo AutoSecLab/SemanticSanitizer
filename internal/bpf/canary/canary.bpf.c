222ca3c9dff558b5dc0744c3ca4f76f0a2f5b808
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2026-04-23 19:23:04 +0200

    perf

--- internal/bpf/canary/canary.bpf.c
+++ internal/bpf/canary/canary.bpf.c
@@ -34,16 +34,44 @@ struct {
   __uint(max_entries, 400);
 } canaries SEC(".maps");
 
-static __always_inline int canary_filter(struct context *sctx) {
-  struct canary_rule *rule = bpf_map_lookup_elem(&canaries, &sctx->syscall_id);
-  if (rule == NULL || *rule->disallowed_str == '\0')
-    return 1;
-
-  __u32 arg_idx = rule->arg_idx;
-  if (arg_idx >= 6)
+SEC("tracepoint/raw_syscalls/sys_enter")
+int canary_filter_wrapper(struct trace_event_raw_sys_enter *raw_ctx) {
+  // Fast path: check if this syscall has a canary rule before doing
+  // expensive work (comm check, arg reads, string matching).
+  long raw_id = BPF_CORE_READ(raw_ctx, id);
+  __u32 syscall_id = (__u32)raw_id;
+  struct canary_rule *rule = bpf_map_lookup_elem(&canaries, &syscall_id);
+  if (rule == NULL || rule->disallowed_str[0] == '\0')
     return 0;
 
-  unsigned long arg_ptr = sctx->args[arg_idx];
+  if (is_expected_comm() != 0)
+    return 0;
+
+  // Read only the argument we need, using constant indices for CO-RE.
+  unsigned long arg_ptr;
+  switch (rule->arg_idx) {
+  case 0:
+    BPF_CORE_READ_INTO(&arg_ptr, raw_ctx, args[0]);
+    break;
+  case 1:
+    BPF_CORE_READ_INTO(&arg_ptr, raw_ctx, args[1]);
+    break;
+  case 2:
+    BPF_CORE_READ_INTO(&arg_ptr, raw_ctx, args[2]);
+    break;
+  case 3:
+    BPF_CORE_READ_INTO(&arg_ptr, raw_ctx, args[3]);
+    break;
+  case 4:
+    BPF_CORE_READ_INTO(&arg_ptr, raw_ctx, args[4]);
+    break;
+  case 5:
+    BPF_CORE_READ_INTO(&arg_ptr, raw_ctx, args[5]);
+    break;
+  default:
+    return 0;
+  }
+
   if (arg_ptr == 0)
     return 0;
 
@@ -55,22 +83,10 @@ static __always_inline int canary_filter(struct context *sctx) {
   int pos = _strstr(buf, rule->disallowed_str, MAX_ARGSTRING_LEN);
   if (pos >= 0) {
     bpf_printk(
-        "canary: detected disallowed substring %s in arg %d of syscall %ld\n",
-        rule->disallowed_str, arg_idx, sctx->syscall_id);
+        "canary: detected disallowed substring %s in arg %d of syscall %u\n",
+        rule->disallowed_str, rule->arg_idx, syscall_id);
     term_action();
-    return 0;
   }
 
   return 0;
-}
-
-SEC("tracepoint/raw_syscalls/sys_enter")
-int canary_filter_wrapper(struct trace_event_raw_sys_enter *raw_ctx) {
-  if (is_expected_comm() != 0)
-    return 1;
-
-  struct context sctx;
-  INIT_SYSCALL_CTX(&sctx, raw_ctx);
-
-  return canary_filter(&sctx);
 }
